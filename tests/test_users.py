8612fd331430d3f26336de149d308b5cc26b10d5
Junior Carlos <junior.carlos@g.globo>
2024-08-07 16:01:40 -0300

    feat: adding token refresh and authorization testings.

--- tests/test_users.py
+++ tests/test_users.py
@@ -66,3 +66,26 @@ def test_delete_user_not_found(client, token):
     response = client.delete('/users/100', headers={'Authorization': f'Bearer {token}'})
 
     assert response.status_code == HTTPStatus.FORBIDDEN
+
+
+def test_update_user_with_wrong_user(client, other_user, token):
+    response = client.put(
+        f'/users/{other_user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.json() == {'detail': 'Not enough permissions'}
+
+
+def test_delete_user_wrong_user(client, other_user, token):
+    response = client.delete(
+        f'/users/{other_user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.json() == {'detail': 'Not enough permissions'}
